c157a58a7261eaa3eb4889baaec321af59857060
Eric Coe <coeeri001@warren.k12.in.us>
2024-09-06 13:18:33 -0400

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,27 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+    }
+
+    private static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+        while (number > 0) {
+            int num = number % 10;
+            if (num % 2 == 0) {
+                sum = sum + num;
+            }
+            number /= 10;
+        }
+
+
+        return sum;
+
+
+    }
 }
